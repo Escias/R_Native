f0dc17d5e8985cc437b3684d8d5db3935492abe1
Jimmy <ljimmy98@hotmail.fr>
2021-12-03 13:11:45 +0100

    adapt display description with new back button

--- components/List/PokemonDescription.js
+++ components/List/PokemonDescription.js
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     height: '95%',
     margin: 10,
-    marginLeft: 100,
+    marginLeft: 35,
     marginRight: 100,
   },
   sprites: {
